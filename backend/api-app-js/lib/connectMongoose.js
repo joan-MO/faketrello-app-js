5ed26d7c0bc716c51f0ef286903aee9d91a03d34
Joan M.O <joanmiquelortega@gmail.com>
2021-03-10 10:29:13 +0100

    create file js to connect mongoDB

---
+++ backend/api-app-js/lib/connectMongoose.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const mongoose = require('mongoose');
+
+mongoose.connection.on('error', err => {
+    console.log('Error connection', err);
+    process.exit(1);
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Connect to MongoDB in', mongoose.connection.name);
+})
+
+mongoose.connect('mongodb://localhost:27017/faketrello', {useNewUrlParser: true, useUnifiedTopology: true});
+
+module.exports = mongoose.connection;
