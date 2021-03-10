d11ead3f64b8e9a8b87d56b62e476387172263e9
Joan M.O <joanmiquelortega@gmail.com>
2021-03-10 10:53:16 +0100

    add path router board and change url to response with json format

--- backend/api-app-js/app.js
+++ backend/api-app-js/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var boardRouter = require('./routes/boardRouter') 
 
 var app = express();
 
@@ -20,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +34,17 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+
+  if (isAPIRequest(req)) {
+    return res.json({ error: err.message });
+  }
+
   res.render('error');
 });
+
+function isAPIRequest(req) {
+  return req.originalUrl.indexOf('/api/') === 0;
+}
+
 
 module.exports = app;
