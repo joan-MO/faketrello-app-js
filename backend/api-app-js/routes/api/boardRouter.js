759b50419d9a0e2f559f0add798c747410c3854c
Joan M.O <joanmiquelortega@gmail.com>
2021-03-11 11:39:05 +0100

    method arrangement and added the other methods

--- backend/api-app-js/routes/api/boardRouter.js
+++ backend/api-app-js/routes/api/boardRouter.js
@@ -1,22 +1,125 @@
 var express = require('express');
 const Board = require('../../models/board');
+const boardSchemaValidation = require('../../utils/schemas/board')
 var router = express.Router();
 const validate = require('../../utils/validateData');
 
 
 router.get('/', async (req, res, next) => {
-  const result = await Board.find();
-  res.json(result);
+
+  try {
+
+    const title = req.query.title;
+
+    const filter = {};
+
+    if (title) {
+      filter.title =  new RegExp('^'
+      +title, "i");
+    }
+
+    const board = await Board.find(filter);
+
+  if (!board) {
+    return res.status(404).json({ error: 'not found' });
+  }
+
+  res.json(board);
+
+  } catch (error) {
+    next(error);
+  }
+  
 });
 
-router.post('/', validate(Board), async (req, res, next) => {
-  const { title } = req.body;
-  const newBoard = new Board ({title});
+router.get('/:_id', async (req, res, next) => {
+  try {
+    const _id = req.params._id;
 
-  const boardCreate = await newBoard.save();
+    const board = await Board.findOne({ _id: _id });
 
-  res.status(201).json({ result: newBoard});
+    if (!board) {
+      return res.status(404).json({ error: 'not found' });
+    }
+    res.json({ result: board });
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+router.post('/', validate(boardSchemaValidation), async (req, res, next) => {
+
+  try {
+
+    const { title } = req.body;
+    const newBoard = new Board ({title});
+  
+    const boardCreate = await newBoard.save();
+  
+    res.status(201).json({ result: newBoard});
+
+  } catch (error) {
+    next(error);
+  }
 
 })
+
+router.put('/:_id', validate(boardSchemaValidation), async(req, res, next) => {
+
+  try {
+    const { _id } = req.params;
+    const { title } = req.body;
+    const updatedBoard = await Board.findByIdAndUpdate(_id, {$set:{ title }}, { useFindAndModify: false} )
+    
+    if (!updatedBoard) {
+      res.status(404).json({ error: 'not found' });
+      return;
+    }
+  
+    res.json({ result: updatedBoard.title });
+  
+  } catch (error) {
+    next(error);
+  }
+
+})
+
+router.put('/assignListTask/:_id', async (req, res) => {
+
+  try {
+
+    const { _id } = req.params;
+    const { list_task } = req.body;
+    const updatedBoard = await studentsModel.findByIdAndUpdate(_id, {$push: { list_task: list_task}}, { useFindAndModify: false})
+
+    if (!updatedBoard) {
+      res.status(404).json({ error: 'not found' });
+      return;
+    }
+  
+    res.json({ result: updatedBoard.title });
+    
+  } catch (error) {
+    next(error); 
+  }
+
+});
+
+router.delete('/:_id', async (req, res, next) => {
+  try {
+
+    const { _id } = req.params;
+    const deletedBoard = await Board.findByIdAndDelete(_id);
+    res.json(`${deletedBoard.title} deleted`);
+
+  } catch (error) {
+    next(error);
+  }
+
+});
+
+
 
 module.exports = router;
--- backend/api-app-js/utils/validateData.js
+++ backend/api-app-js/utils/validateData.js
@@ -2,6 +2,7 @@ const Boom = require('@hapi/boom')
 module.exports = (schema) => {
     return async (req, res, next) => {
         try {
+        console.log(schema);
         await schema.validateAsync(req.body);
         next();
         } catch (error) {
