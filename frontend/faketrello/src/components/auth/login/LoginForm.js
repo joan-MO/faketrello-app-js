dc751e3cae16283dd9cd5e0005be20bdc34e77da
Joan M.O <joanmiquelortega@gmail.com>
2021-04-12 16:24:03 +0200

    create the first logic login page

--- frontend/faketrello/src/components/auth/login/LoginForm.js
+++ frontend/faketrello/src/components/auth/login/LoginForm.js
@@ -1,21 +1,63 @@
 import React from 'react';
 
-const LoginForm = () => {
+const LoginForm = ({onSubmit}) => {
+    const [credentials, setCredentials] = React.useState({
+        email: '',
+        password: '',
+        checked: ''
+    })
+
+    const changeEmail = event => {
+        const newCredentials = { ...credentials, email: event.target.value };
+        setCredentials(newCredentials);
+    };
+
+    const changePassword = event => {  
+        const newCredentials = { ...credentials, password: event.target.value};
+        setCredentials(newCredentials);
+    };
+    
+    const changeChecked = event => {
+        const newCredentials = { ...credentials, checked: event.target.checked };
+        setCredentials(newCredentials);
+    };
+
+    const submitForm = event => {
+        event.preventDefault();
+        onSubmit(credentials);
+    };
+
+
+    const { email, password } = credentials;
+
     return (
-        <form>
+        <form onSubmit={submitForm}> 
             <div className="form-group mt-2">
                 <label htmlFor="email">Email address</label>
-                <input type="email" name="email" className="form-control mt-2" id="email" aria-describedby="emailHelp" placeholder="enter email" />
+                <input type="email"
+                 name="email" 
+                 className="form-control mt-2" 
+                 id="email"
+                 value={email}
+                 onChange={changeEmail} 
+                 aria-describedby="emailHelp" 
+                 placeholder="enter email" required/>
             </div>
             <div className="form-group mt-2">
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" className="form-control mt-2" id="password" placeholder="password" />
+                <input type="password"
+                 name="password"
+                 className="form-control mt-2" 
+                 id="password"
+                 value={password}
+                 onChange={changePassword} 
+                 placeholder="password" required/>
             </div>
             <div className="form-check mt-2">
-                <input type="checkbox" className="form-check-input" id="check" />
+                <input type="checkbox" className="form-check-input" id="check" onChange={changeChecked}/>
                 <label className="form-check-label" htmlFor="check">Remember password</label>
             </div>
-            <button type="submit" className="btn btn-primary mt-2">Submit</button>
+            <button type="submit" className="btn btn-primary mt-2" disabled={!email || !password}>Submit</button>
         </form>
     )
 }
--- frontend/faketrello/src/components/auth/login/LoginPage.js
+++ frontend/faketrello/src/components/auth/login/LoginPage.js
@@ -3,6 +3,19 @@ import LoginForm from './LoginForm'
 import './Login.css'
 
 const LoginPage = () => {
+    const [error, setError] = React.useState(null);
+
+    const resetError = React.useCallback(() => setError(), []);
+
+    const handleSubmit = async credentials => {
+        resetError();
+        try {
+        alert('login correcto')
+        } catch (error) {
+        setError(error);
+        }
+    };
+    
     return (
         <div className="container">
             <div className="row">
@@ -10,7 +23,13 @@ const LoginPage = () => {
                     <div className="card p-5" style={{width:"25rem",height:'400px', marginTop:'30%'}}>
                         <div className="card-body">
                             <h5 className="card-title text-center mb-4">Login Page</h5>
-                            <LoginForm />
+                            <LoginForm onSubmit={handleSubmit}/>
+                            {error && (
+                                <p className="bg-danger text-center"
+                                style={{ color: "white", position: "relative", top: "150px" }}>
+                                    {error.message}  {error.status}
+                                </p>
+                            )}
                         </div>
                     </div>
                 </div>
