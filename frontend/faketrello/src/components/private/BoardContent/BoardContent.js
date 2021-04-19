bb56a122706a79fa512a72f193620fb3bc85ca38
Joan M.O <joanmiquelortega@gmail.com>
2021-04-19 16:31:47 +0200

    create structure with fake data add new boards

--- frontend/faketrello/src/components/private/BoardContent/BoardContent.js
+++ frontend/faketrello/src/components/private/BoardContent/BoardContent.js
@@ -5,6 +5,7 @@ const BoardContent = () => {
     return (
         <div>
             <Header />
+            
         </div>
     )
 }
--- frontend/faketrello/src/components/shared/cardBoards.js
+++ frontend/faketrello/src/components/shared/cardBoards.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import '../../components/private/boards/boardsList/board.css'
 import { Link } from 'react-router-dom';
+import  { Modal, Button } from 'react-bootstrap'
 
-const CardBoards = ({boards}) => {
+
+const CardBoards = ({ boards }) => {
+    const [show, setShow] = React.useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const [content, setContent] = React.useState({
+        list_tasks: [],
+        _id: 4 ,
+        title: ''
+    });
+
+  
+    const changeTitle = event => {
+        const newContent = { ...content, title: event.target.value };
+        setContent(newContent);
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(content);
+        boards.push(content);
+        handleClose();
+
+    };
+
+    const { title } = content;
     return (
         <div className="row">
             {boards.map(board =>
@@ -17,12 +43,26 @@ const CardBoards = ({boards}) => {
             </div>
             )}
             <div className="col-6">
-                <div className="card btn bg-primary mt-4" style={{ width: "219.2px", height: "96px" }}>
+                <div className="card btn bg-primary mt-4" style={{ width: "219.2px", height: "96px" }} onClick={handleShow}>
                     <div className="card-body">
                         <p className="card-title title-newboard p-3 text-center">add new board</p>
                     </div>
                 </div>
             </div>
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Body>
+                <form onSubmit={handleSubmit} id="myform">
+                    <label htmlFor="name">Title: </label>
+                        <input type="text" name="title" id="title" className="form-control" value={title} onChange={changeTitle}/>
+                </form>
+            </Modal.Body>
+            <Modal.Footer>
+              <input className="btn btn-success" type="submit" form="myform" value="SEND"/>
+              <Button variant="secondary" onClick={handleClose}>
+                CANCELAR
+              </Button>
+            </Modal.Footer>
+          </Modal>
             
         </div>
     )
