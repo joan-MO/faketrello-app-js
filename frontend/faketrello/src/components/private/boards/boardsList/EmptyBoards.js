6d9f974debccbfc4f1271716d02ae78b7be89548
Joan M.O <joanmiquelortega@gmail.com>
2021-04-29 15:09:25 +0200

    add empty boards component

--- frontend/faketrello/src/components/private/boards/boardsList/EmptyBoards.js
+++ frontend/faketrello/src/components/private/boards/boardsList/EmptyBoards.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import './board.css'
+import NewBoards from '../newBoards/NewBoards'
+import { createBoard } from '../../../../api/service'
+import { Redirect, useHistory } from 'react-router-dom';
 
 const EmptyBoard = () => {
+    const [show, setShow] = React.useState(false);
+    const [error, setError] = React.useState(null);
+    //const [createdBoard, setCreatedBoard] = React.useState(null);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    //const history = useHistory();
+
+    const handleSubmit = async content => {
+        try {
+
+            await createBoard(content);
+
+            handleClose();
+            
+        } catch (error) {
+            setError(error);
+        }
+        
+    };
+
+    
+    if (error && error.status === 401) {
+       <Redirect to='/login' />
+    }
+    
     return (
-        <div className="col-6 mt-2">
-            <div className="card btn bg-primary" style={{ width: "219.2px", height: "96px" }}>
-                <div className="card-body">
-                    <p className="card-title title-newboard p-3 text-center">add new board</p>
-                </div>
+    <div>
+    <div className="col-6">
+        <div className="card btn bg-primary mt-2" style={{ width: "219.2px", height: "96px" }} onClick={handleShow}>
+            <div className="card-body">
+                <p className="card-title title-newboard p-3 text-center">add new board</p>
             </div>
         </div>
+    </div>
+    <NewBoards handleClose={handleClose} onSubmit={handleSubmit} show={show}/>
+    </div>
     )
 }
 
