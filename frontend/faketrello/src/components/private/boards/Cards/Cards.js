de509982f0f6670c0fb559bc4db8e02ff8c54706
Joan M.O <joanmiquelortega@gmail.com>
2021-05-05 12:01:09 +0200

    test to recover _id card

--- frontend/faketrello/src/components/private/boards/Cards/Cards.js
+++ frontend/faketrello/src/components/private/boards/Cards/Cards.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import NewCards from '../newCards/NewCards';
 import EmptyCard from './EmptyCard'
-const Cards = ({task}) => {
-
+const Cards = ({task, onSubmit}) => {
+    const edit = (id) => {
+        alert('edit' + id)
+    }
     return (
         <div>
         {task.card_tasks.length ? 
         <div>
             {task.card_tasks.map(card=>
-                <p key={card._id}>{card.title}</p>
+                <div>
+                <p key={card._id}>{card.title}</p><p className="btn" onClick={() => edit(card._id)}>Edit</p>
+                </div>
             )}
-           <NewCards _id={task._id}/>
+           <NewCards _id={task._id} onSubmit={onSubmit}/>
         </div>
-        : <EmptyCard _id={task._id}/>}
+        : <EmptyCard _id={task._id} onSubmit={onSubmit}/>}
         </div>
     )
 }
